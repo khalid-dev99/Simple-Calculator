82aa6dc7b6cb885662fbcbaeca3c9e1f3d58fb7b
khalid-dev99 <ourcamel592@gmail.com>
2025-10-09 19:06:56 +0300

    Add files via upload

---
+++ Simple calculator.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std; 
+int main() {
+	char choice;
+	double num1, num2;
+	cout << "Select an opreation to perform the calculator: ";
+	cin >> choice;
+	cout << "Enter Two Digits: ";
+	cin >> num1 >> num2;
+	switch (choice) {
+	case '+':
+		cout << "The Result is: " << num1 + num2 << endl;
+		break;
+	case '-':
+		cout << "The Result is: " << num1 - num2 << endl;
+		break;
+	case '*':
+		cout << "The Result is: " << num1 * num2 << endl;
+		break;
+	case '/':
+		if (num2 != 0) {
+			cout << "The Result is: " << num1 / num2 << endl;
+			break;
+		}
+	default:
+		cout << "Invalid operation" << endl;
+		break;
+	}
+	return 0;
+}
